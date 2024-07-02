26f3cdf8787c93a3ee47b781e4bf742327850a11
Micael Gomes <contato.micaeloficial@gmail.com>
2024-07-02 18:35:15 -0300

    fix: changing main.tsx to accept async User definition

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { AuthContextProvider } from './services/supabase/AuthContext.tsx';
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
 import '../app/globals.css';
-
 import { getUserService } from './services/supabase/User/get-user.ts';
-const user = await getUserService();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <AuthContextProvider initialUser={user} >
-        <App />
+async function initializeApp() {
+  const user = await getUserService();
+
+  (ReactDOM as any).createRoot(document.getElementById('root')!).render(
+    <AuthContextProvider initialUser={user}>
+      <App />
     </AuthContextProvider>
-)
+  );
+}
+
+initializeApp();
