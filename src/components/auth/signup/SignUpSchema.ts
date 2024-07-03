b226e169d78e36b3ac12367d58fc5f21b3b9a32b
Micael Gomes <contato.micaeloficial@gmail.com>
2024-07-03 17:09:16 -0300

    Fix: removing avatar_url

--- src/components/auth/signup/SignUpSchema.ts
+++ src/components/auth/signup/SignUpSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const signUpSchema = z.object({
   email: z.string().email({ message: "Email inválido" }),
   password: z.string().min(6, { message: "A senha precisa ter no mínimo 6 caracteres" }),
-  username: z.string().min(6, { message: "O nome de usuário precisa ter no mínimo 6 caracteres" })
+  username: z.string().min(6, { message: "O nome de usuário precisa ter no mínimo 6 caracteres" }),
 });
 
 export type SignUpFormInputs = z.infer<typeof signUpSchema>;
--- src/types/user-profile.ts
+++ src/types/user-profile.ts
@@ -1,7 +1,6 @@
 export interface Profile {
   id: string;
   username: string;
-  avatar_url: string;
   created_at: string;
   updated_at: string;
 }
