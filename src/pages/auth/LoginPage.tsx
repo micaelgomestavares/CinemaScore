b1dfb2dac012ee2685318d42e1d26dacd137c96b
Micael Gomes <contato.micaeloficial@gmail.com>
2024-07-08 01:13:27 -0300

    feat: adding a card around login and signup forms

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,22 +1,39 @@
 
 import LoginForm from "@/components/auth/login/LoginForm";
 import SignUpForm from "@/components/auth/signup/SignUpForm";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const LoginPage: React.FC<{}> = () => {
   return (
     <main className="w-full">
-      <section className="mx-auto my-4 w-full max-w-4xl mt-8 max-lg:p-6">
-        <Tabs defaultValue="login">
+      <section className="flex justify-center mx-auto my-4 w-full max-w-4xl mt-8 max-lg:p-6">
+        <Tabs className="w-[500px]" defaultValue="login">
           <TabsList>
             <TabsTrigger value="login">Login</TabsTrigger>
             <TabsTrigger value="signup">Cadastro</TabsTrigger>
           </TabsList>
           <TabsContent value="login">
-            <LoginForm></LoginForm>
+            <Card>
+              <CardHeader>
+                <CardTitle>Entrar</CardTitle>
+                <CardDescription>Realize login na plataforma.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <LoginForm></LoginForm>
+              </CardContent>
+            </Card>
           </TabsContent>
           <TabsContent value="signup">
-            <SignUpForm></SignUpForm>
+            <Card>
+              <CardHeader>
+                <CardTitle>Cadastro</CardTitle>
+                <CardDescription>Cadastre-se e tenha acesso a todos as funcionalidades da plataforma.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <SignUpForm></SignUpForm>
+              </CardContent>
+            </Card>
           </TabsContent>
         </Tabs>
       </section>
