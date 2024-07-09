c978d4258a99c916ef75768eee55de9e74fbf18c
Micael Gomes <contato.micaeloficial@gmail.com>
2024-07-09 00:39:37 -0300

    fix: fixing hitbox on icon mode-toggle

--- src/components/theme/mode-toggle.tsx
+++ src/components/theme/mode-toggle.tsx
@@ -1,15 +1,20 @@
-import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import { useTheme } from "@/providers/theme-provider"
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/providers/theme-provider";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
-    <Button variant="outline" size="icon">
-      <Sun onClick={() => setTheme("dark")} className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon onClick={() => setTheme("light")} className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <Button variant="outline" size="icon" className="relative">
+      <Sun 
+        onClick={() => setTheme("dark")} 
+        className="absolute inset-0 h-[1.2rem] w-[1.2rem] m-auto transition-all dark:-rotate-90 dark:scale-0" 
+      />
+      <Moon 
+        onClick={() => setTheme("light")} 
+        className="absolute inset-0 h-[1.2rem] w-[1.2rem] m-auto rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" 
+      />
     </Button>
-  )
+  );
 }
